205f636f8a8b19ab04fffbd3bdde45c342cdec1c
42040 <https://github.com/SpeedataApp/kuaishouExample.git>
2018-08-08 15:12:21 +0800

    tw：增加新上电路径例如sd55，sk80设备调整主板上电可以多个

--- deivice/src/main/java/android/serialport/DeviceControl.java
+++ deivice/src/main/java/android/serialport/DeviceControl.java
@@ -7,11 +7,19 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * 注意：
+ * 无论新老设备，操作主板gpio， 需要设置gpio模式和输出模式，代码调用需要先设置为gpio模式再设置输出模式再讲gpio拉高
+ */
 public class DeviceControl {
     //kt系列
     public static final String POWER_MAIN = "/sys/class/misc/mtgpio/pin";
     //tt系列
     public static final String POWER_EXTERNAL = "/sys/class/misc/aw9523/gpio";
+    //sk80 新添加
+    public static final String POWER_EXTERNAL2 = "/sys/class/misc/aw9524/gpio";
+    //新设备上电路径
+    public static final String POWER_NEWMAIN = "/sys/bus/platform/drivers/mediatek-pinctrl/10005000.pinctrl/mt_gpio";
 
     /**
      * 上电类型
@@ -28,7 +36,21 @@ public class DeviceControl {
         /**
          * 主板和外部扩展
          */
-        MAIN_AND_EXPAND
+        MAIN_AND_EXPAND,
+        /**
+         * 新设备 sd55上电
+         */
+        NEW_MAIN,
+        /**
+         * 9524上电sk80使用
+         */
+        EXPAND2,
+        /**
+         * 主板和9524上电
+         */
+        MAIN_AND_EXPAND2
+
+
     }
 
     private BufferedWriter CtrlFile;
@@ -36,19 +58,27 @@ public class DeviceControl {
     private String poweron = "";
     private String poweroff = "";
     private String currentPath = "";
+
+    public DeviceControl() throws IOException {
+
+    }
+
     public DeviceControl(String path) throws IOException {
         File DeviceName = new File(path);
         CtrlFile = new BufferedWriter(new FileWriter(DeviceName, false));    //open file
         currentPath = path;
     }
 
+
+    private int[] gpios;
+
     /**
      * 此方法可单独设置gpio
-     * @param gpio 设置gpio
      *
+     * @param gpio 设置gpio
      */
     public void setGpio(int gpio) {
-        if (currentPath.equals(POWER_EXTERNAL)) {
+        if (currentPath.equals(POWER_EXTERNAL) || currentPath.equals(POWER_EXTERNAL2)) {
             poweron = gpio + "on";
             poweroff = gpio + "off";
         } else {
@@ -59,13 +89,11 @@ public class DeviceControl {
         }
     }
 
-    int[] gpios;
     private PowerType power_type;
 
     /**
-     *
      * @param power_type 上电类型
-     * @param gpios 若为主板上电 gpio[0]需为主板gpio 扩展gpio可以有多个
+     * @param gpios      若为主板上电 gpio[0]需为主板gpio 扩展gpio可以有多个
      * @throws IOException
      */
     public DeviceControl(PowerType power_type, int... gpios) throws IOException {
@@ -74,9 +102,8 @@ public class DeviceControl {
     }
 
     /**
-     *
      * @param power_type 上电类型
-     * @param gpios 若为主板上电 gpio[0]需为主板gpio 扩展gpio可以有多个
+     * @param gpios      若为主板上电 gpio[0]需为主板gpio 扩展gpio可以有多个
      * @throws IOException
      */
     public DeviceControl(String power_type, int... gpios) throws IOException {
@@ -91,11 +118,22 @@ public class DeviceControl {
             case "MAIN_AND_EXPAND":
                 this.power_type = PowerType.MAIN_AND_EXPAND;
                 break;
+            case "NEW_MAIN":
+                this.power_type = PowerType.NEW_MAIN;
+                break;
+            case "EXPAND2":
+                this.power_type = PowerType.EXPAND2;
+                break;
+            case "MAIN_AND_EXPAND2":
+                this.power_type = PowerType.MAIN_AND_EXPAND2;
+                break;
+
         }
     }
 
     /**
      * 主板上电
+     *
      * @param gpio
      * @throws IOException
      */
@@ -108,6 +146,7 @@ public class DeviceControl {
 
     /**
      * 主板下电
+     *
      * @param gpio
      * @throws IOException
      */
@@ -120,6 +159,7 @@ public class DeviceControl {
 
     /**
      * 外部扩展上电
+     *
      * @param gpio
      * @throws IOException
      */
@@ -130,8 +170,16 @@ public class DeviceControl {
         deviceControl.DeviceClose();
     }
 
+    public void Expand2PowerOn(int gpio) throws IOException {
+        DeviceControl deviceControl = new DeviceControl(DeviceControl.POWER_EXTERNAL2);
+        deviceControl.setGpio(gpio);
+        deviceControl.writeON();
+        deviceControl.DeviceClose();
+    }
+
     /**
      * 外部扩展下电
+     *
      * @param gpio
      * @throws IOException
      */
@@ -142,9 +190,17 @@ public class DeviceControl {
         deviceControl.DeviceClose();
     }
 
+    public void Expand2PowerOff(int gpio) throws IOException {
+        DeviceControl deviceControl = new DeviceControl(DeviceControl.POWER_EXTERNAL2);
+        deviceControl.setGpio(gpio);
+        deviceControl.WriteOff();
+        deviceControl.DeviceClose();
+    }
+
 
     /**
      * 写ON
+     *
      * @throws IOException
      */
     private void writeON() throws IOException {
@@ -154,6 +210,7 @@ public class DeviceControl {
 
     /**
      * 写off
+     *
      * @throws IOException
      */
     private void WriteOff() throws IOException {
@@ -163,25 +220,44 @@ public class DeviceControl {
 
     /**
      * 构造函数之后 可带调用此方法上电
+     *
      * @throws IOException 抛出异常
      */
     public void PowerOnDevice() throws IOException        //poweron id device
     {
         switch (power_type) {
             case MAIN:
-                MainPowerOn(gpios[0]);
-                SystemClock.sleep(200);
+                for (int i = 0; i < gpios.length; i++) {
+                    MainPowerOn(gpios[i]);
+                    SystemClock.sleep(100);
+                }
                 break;
             case EXPAND:
-                ExpandPowerOn(gpios[0]);
-                SystemClock.sleep(200);
+                for (int i = 0; i < gpios.length; i++) {
+                    ExpandPowerOn(gpios[i]);
+                    SystemClock.sleep(100);
+                }
                 break;
             case MAIN_AND_EXPAND:
                 MainPowerOn(gpios[0]);
-                SystemClock.sleep(200);
+                SystemClock.sleep(100);
                 for (int i = 1; i < gpios.length; i++) {
                     ExpandPowerOn(gpios[i]);
-                    SystemClock.sleep(200);
+                    SystemClock.sleep(100);
+                }
+                break;
+            case NEW_MAIN:
+                for (int i = 0; i < gpios.length; i++) {
+                    newSetGpioOn(gpios[i]);
+                    SystemClock.sleep(100);
+                }
+                break;
+            case MAIN_AND_EXPAND2:
+                MainPowerOn(gpios[0]);
+                SystemClock.sleep(100);
+                for (int i = 1; i < gpios.length; i++) {
+                    Expand2PowerOn(gpios[i]);
+                    SystemClock.sleep(100);
                 }
                 break;
             default:
@@ -192,21 +268,39 @@ public class DeviceControl {
 
     /**
      * 构造函数后 程序退出时可调用此方法下电
+     *
      * @throws IOException 抛出异常
      */
     public void PowerOffDevice() throws IOException        //poweroff id device
     {
         switch (power_type) {
             case MAIN:
-                MainPowerOff(gpios[0]);
+                for (int i = 0; i < gpios.length; i++) {
+                    MainPowerOff(gpios[i]);
+                    SystemClock.sleep(100);
+                }
                 break;
             case EXPAND:
-                ExpandPowerOff(gpios[0]);
+                for (int i = 0; i < gpios.length; i++) {
+                    ExpandPowerOff(gpios[i]);
+                    SystemClock.sleep(100);
+                }
                 break;
             case MAIN_AND_EXPAND:
                 MainPowerOff(gpios[0]);
                 for (int i = 1; i < gpios.length; i++) {
                     ExpandPowerOff(gpios[i]);
+                }
+                break;
+            case MAIN_AND_EXPAND2:
+                MainPowerOff(gpios[0]);
+                for (int i = 1; i < gpios.length; i++) {
+                    Expand2PowerOff(gpios[i]);
+                }
+                break;
+            case NEW_MAIN:
+                for (int i = 0; i < gpios.length; i++) {
+                    newSetGpioOff(gpios[i]);
                 }
                 break;
             default:
@@ -216,6 +310,7 @@ public class DeviceControl {
 
     /**
      * 关闭文件
+     *
      * @throws IOException
      */
     public void DeviceClose() throws IOException        //close file
@@ -225,7 +320,8 @@ public class DeviceControl {
 
     /**
      * 设置制定管脚模式  0为gpio模式
-     * @param num 管脚
+     *
+     * @param num  管脚
      * @param mode 0为gpio模式
      * @throws IOException
      */
@@ -236,12 +332,13 @@ public class DeviceControl {
 
     /**
      * 设置输入输出模式
+     *
      * @param num  管脚
-     * @param mode  输入输出模式
-     * @param path  路径
+     * @param mode 输入输出模式
+     * @param path 路径
      * @throws IOException
      */
-    public void setDir(int num, int mode,String path) throws IOException {
+    public void setDir(int num, int mode, String path) throws IOException {
         File DeviceName = new File(path);
         CtrlFile = new BufferedWriter(new FileWriter(DeviceName, false));    //open file
         CtrlFile.write("-wdir" + num + " " + mode);   //设置为输入输出
@@ -250,17 +347,75 @@ public class DeviceControl {
 
     /**
      * 设置上拉下拉
+     *
      * @param num
      * @param mode
      * @param path
      * @throws IOException
      */
-    public void setPull(int num, int mode,String path)throws IOException {
+    public void setPull(int num, int mode, String path) throws IOException {
         File DeviceName = new File(path);
         CtrlFile = new BufferedWriter(new FileWriter(DeviceName, false));    //open file
         CtrlFile.write("-wpsel" + num + " " + mode);   //设置为输入输出
         CtrlFile.flush();
         CtrlFile.write("-wpen" + num + " " + mode);   //设置为输入输出
         CtrlFile.flush();
+    }
+
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    /**
+     * 设置新设备上电 例如sd55 sd60
+     *
+     * @param gpio
+     * @throws IOException
+     */
+    public void newSetGpioOn(int gpio) throws IOException {
+        DeviceControl deviceControl = new DeviceControl(POWER_NEWMAIN);
+        deviceControl.CtrlFile.write("out " + gpio + " 1");//将GPIO设置为高电平
+        deviceControl.CtrlFile.flush();
+        newSetMode(gpio);
+        newSetDir(gpio, 1);
+
+    }
+
+    /**
+     * 设置新设备下点 例如sd55 sd60
+     *
+     * @param gpio
+     * @throws IOException
+     */
+    public void newSetGpioOff(int gpio) throws IOException {
+        DeviceControl deviceControl = new DeviceControl(POWER_NEWMAIN);
+        deviceControl.CtrlFile.write("out " + gpio + " 0");//将GPIO设置为低电平
+        deviceControl.CtrlFile.flush();
+
+    }
+
+    /**
+     * 设置gpio为 gpio模式 例如sd55 sd60
+     *
+     * @param gpio
+     * @throws IOException
+     */
+    public void newSetMode(int gpio) throws IOException {
+        DeviceControl deviceControl = new DeviceControl(POWER_NEWMAIN);
+        deviceControl.CtrlFile.write("mode " + gpio + " 0");//将GPIO设置为GPIO模式
+        deviceControl.CtrlFile.flush();
+
+    }
+
+    /**
+     * 设置gpio为输入/输出模式 例如sd55 sd60
+     *
+     * @param gpio 要操作的gpio
+     * @param dir  0：输入模式  1：输出模式
+     * @throws IOException
+     */
+    public void newSetDir(int gpio, int dir) throws IOException {
+        DeviceControl deviceControl = new DeviceControl(POWER_NEWMAIN);
+        deviceControl.CtrlFile.write("dir " + gpio + " " + dir);//将GPIO99设置为输出模式
+        deviceControl.CtrlFile.flush();
+
     }
 }
--- deivice/src/main/java/android/serialport/SerialPort.java
+++ deivice/src/main/java/android/serialport/SerialPort.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2009 Cedric Priscal
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 
 package android.serialport;
@@ -286,7 +286,7 @@ public class SerialPort {
         }
         /*
          * for(byte x : tmp) { Log.w("xxxx", String.format("0x%x", x)); }
-		 */
+         */
 
         return tmp;
     }
